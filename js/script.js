3212d86f8344c8b8c52f52f1dc32f2a12d21bc45
Johannes <polyhickup@gmail.com>
2015-01-27 16:11:22 +0100

    .

--- js/script.js
+++ js/script.js
@@ -40,6 +40,9 @@ function loadData() {
             var article = articles[i];
             $nytElem.append('<li class="article">' + '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+'<p>' + article.snippet + '</p>' + '</li>');
         }
+    })
+    .error(function() {
+        $nytHeaderElem.text("New York Times Articles Could Not Be Loaded.");
     });
 
     return false;
